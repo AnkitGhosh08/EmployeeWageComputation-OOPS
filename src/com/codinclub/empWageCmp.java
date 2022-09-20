31ec5eec8f95a2dcc0caa2b573c334d2d69e2eab
Ankit Ghosh <ankitghosh877@gmail.com>
2022-09-20 16:34:32 +0530

    switch case statement

--- src/com/codinclub/empWageCmp.java
+++ src/com/codinclub/empWageCmp.java
@@ -5,25 +5,24 @@ public class empWageCmp
     int isFullTime = 1;
     int isPartTime = 2;
     int empRatePerHrs = 20;
-    int empHrs = 8;
-    int empWage=0;
-
+    int workingHours = 0;
+    int wage = 0;
     public void dailyWage()
     {
-
         int empCheck = (int) (Math.floor (Math.random() * 10) % 2);
-
-        if (empCheck == isFullTime) {
-            System.out.println("Employee is present");
-            empHrs = 8;
-        }else if( empCheck == isPartTime) {
-            System.out.println("Employee is present");
-            empHrs = 4;
-        } else {
-            System.out.println("Employee is absent");
-            empHrs = 0;
+        switch (empCheck){
+            case 1:
+                System.out.println("Employee is present");
+                workingHours = 8;
+                break;
+            case 2:
+                System.out.println("Employee is present");
+                workingHours = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
         }
-        empWage = (empHrs * empRatePerHrs);
-        System.out.println("employee daily wage is :" + empWage);
+        wage = workingHours * empRatePerHrs;
+        System.out.println("Employee Daily Wage is : " + wage);
     }
 }
